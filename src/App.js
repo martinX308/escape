3d9ce0559095ed2837eee3f199374c8e0e751cf2
Martin <mar.gardemann@gmail.com>
2020-03-19 21:40:15 +0100

    Update app, sign in,up component with hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import { Switch, Route, Redirect} from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -10,8 +10,6 @@ import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
 
-import {auth, createUserProfileDocument, addCollectionAndDocuments} from './firebase/firebase.utils';
-
 import { checkUserSession } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { SelectCollectionsForPreview } from './redux/shop/shop.selectors';
@@ -19,61 +17,39 @@ import { SelectCollectionsForPreview } from './redux/shop/shop.selectors';
 import './App.css';
 
 
-class App extends React.Component {
+const App = ({checkUserSession,currentUser}) => {
 
-  unsubscribeFromAuth = null;
+  useEffect(()=>{
+    checkUserSession()
+  },[checkUserSession]);
+  
 
-  componentDidMount() {
-    const { checkUserSession } = this.props;
-    checkUserSession();
-    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-    //   if (userAuth) {
-    //     const userRef = await createUserProfileDocument(userAuth);
 
-    //     userRef.onSnapshot( snapShot => {
-    //       setCurrentUser({
-    //         id: snapShot.id,
-    //         ...snapShot.data()
-    //       });
-    //     });
-    //   } else {
-    //     setCurrentUser(userAuth);
-    //     // addCollectionAndDocuments(
-    //     //   'collections',
-    //     //   collectionsArray.map(({title, items}) => ({title, items}))
-    //     // );
-    //   }
-    // })
-  }
+ 
 
-  componentWillUnmount () {
-    this.unsubscribeFromAuth();
-  }
+  return (
+    <div >
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route exact path='/checkout' component={CheckoutPage} />
+        <Route 
+          exact 
+          path='/signin' 
+          render={() => 
+            currentUser ? (
+              <Redirect to='/'/>
+            ):(
+              <SignInAndSignUpPage/>
+            )
+          }
+        />
+      </Switch>
+    </div>
+  );
 
-  render () {
-    return (
-      <div >
-        <Header/>
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route exact path='/checkout' component={CheckoutPage} />
-          <Route 
-            exact 
-            path='/signin' 
-            render={() => 
-              this.props.currentUser ? (
-                <Redirect to='/'/>
-              ):(
-                <SignInAndSignUpPage/>
-              )
-            }
-          />
-        </Switch>
-      </div>
-    );
 
-  }
 }
 
 const mapStateToProps = createStructuredSelector ({
--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -9,24 +9,17 @@ import { signUpStart } from '../../redux/user/user.actions';
 
 import './sign-up.styles.scss';
 
-class SignUp extends React.Component {
-  constructor() {
-    super();
+const SignUp = ({ signUpStart }) => {
+  const [userCredentials,setCredentials] = useState({      
+    displayName:'',
+    email:'',
+    password:'',
+    confirmPassword:''
+  });
+  const { displayName, email, password, confirmPassword} = userCredentials;
 
-    this.state = {
-      displayName:'',
-      email:'',
-      password:'',
-      confirmPassword:''
-    }
-  }
-
-  handleSubmit = async event => {
+  const handleSubmit = async event => {
     event.preventDefault();
-
-    const { displayName, email, password, confirmPassword} = this.state;
-    const {signUpStart} = this.props;
-
 
     if(password !== confirmPassword) {
       alert("passwords don't match");
@@ -36,56 +29,54 @@ class SignUp extends React.Component {
 
   }
 
-  handleChange = event => {
+  const handleChange = event => {
     const {name, value} = event.target;
-    this.setState({[name]:value});
+    setCredentials({...userCredentials,[name]:value});
   }
 
-  render() {
-    const { displayName, email, password, confirmPassword} = this.state;
-    return(
-      <div className='sign-up'>
-        <h2 className='title'>I do not have an account</h2>
-        <span>Sign up with your email and password</span>
-        <form className='sign-up-form' onSubmit={this.handleSubmit}>
-          <FormInput 
-            type='text'
-            name='displayName'
-            value={displayName}
-            onChange={this.handleChange}
-            label='Display Name'
-            required
-          />
-          <FormInput 
-            type='email'
-            name='email'
-            value={email}
-            onChange={this.handleChange}
-            label='Email'
-            required
-          />
-          <FormInput 
-            type='password'
-            name='password'
-            value={password}
-            onChange={this.handleChange}
-            label='Password'
-            required
-          />
-          <FormInput 
-            type='password'
-            name='confirmPassword'
-            value={confirmPassword}
-            onChange={this.handleChange}
-            label='Confirm Password'
-            required
-          />
-          <CustomButton type='submit'>SIGN UP</CustomButton>
-        </form>
-      </div>
-    )
-  }
+  return(
+    <div className='sign-up'>
+      <h2 className='title'>I do not have an account</h2>
+      <span>Sign up with your email and password</span>
+      <form className='sign-up-form' onSubmit={handleSubmit}>
+        <FormInput 
+          type='text'
+          name='displayName'
+          value={displayName}
+          onChange={handleChange}
+          label='Display Name'
+          required
+        />
+        <FormInput 
+          type='email'
+          name='email'
+          value={email}
+          onChange={handleChange}
+          label='Email'
+          required
+        />
+        <FormInput 
+          type='password'
+          name='password'
+          value={password}
+          onChange={handleChange}
+          label='Password'
+          required
+        />
+        <FormInput 
+          type='password'
+          name='confirmPassword'
+          value={confirmPassword}
+          onChange={handleChange}
+          label='Confirm Password'
+          required
+        />
+        <CustomButton type='submit'>SIGN UP</CustomButton>
+      </form>
+    </div>
+  )
 }
+
 
 const mapDispatchToProps = dispatch => ({
   signUpStart: (userCredentials) => dispatch(signUpStart(userCredentials))
